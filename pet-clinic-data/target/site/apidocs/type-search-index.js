37dcb2fe230096801602b230ac326e63dc99f2bd
Weypher <48047975+Weypher@users.noreply.github.com>
2020-03-16 23:37:22 -0700

    some error in issue #3. closes #3

---
+++ pet-clinic-data/target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"guru.springframework.model","l":"Owner"},{"p":"guru.springframework.model","l":"Person"},{"p":"guru.springframework.model","l":"Pet"},{"p":"guru.springframework.model","l":"PetType"},{"p":"guru.springframework.model","l":"Vet"}]
